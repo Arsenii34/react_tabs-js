b56a9ed85313b6bce59956bc06b3fefca7d61aea
Arseniy Naumov <ivanov262ivanov@gmail.com>
2026-01-30 12:43:31 +0200

    add task solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 import './App.scss';
@@ -9,35 +9,26 @@ export const tabs = [
   { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
 ];
 
+
 export const App = () => (
+  const [tabses setTabses] = useState('');
   <div className="section">
-    <h1 className="title">Selected tab is Tab 1</h1>
+    <h1 className="title">Selected tab is {tabses.title}</h1>
 
     <div data-cy="TabsComponent">
       <div className="tabs is-boxed">
         <ul>
-          <li className="is-active" data-cy="Tab">
-            <a href="#tab-1" data-cy="TabLink">
-              Tab 1
-            </a>
-          </li>
+          <Tabs
+            tabs={tabs}
+            activeTabId={activeTabId}
+            onTabSelected={handleSelect}
+          />
 
-          <li data-cy="Tab">
-            <a href="#tab-2" data-cy="TabLink">
-              Tab 2
-            </a>
-          </li>
-
-          <li data-cy="Tab">
-            <a href="#tab-3" data-cy="TabLink">
-              Tab 3
-            </a>
-          </li>
         </ul>
       </div>
 
       <div className="block" data-cy="TabContent">
-        Some text 1
+        {tabses.content}
       </div>
     </div>
   </div>
--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,15 @@
-export const Tabs = () => {};
+export const Tabs = (tabs, activeTabId, handleSelect) => {
+  {
+    tabs.map(tab => (
+      <li
+       className={activeTabId === tab.id?'is-active':''}
+       data-cy="Tab"
+       key={tab.id}
+       >
+        <a onClick={() => tab.id !== activeTabId?handleSelect(tab.id):null}
+         ref={`#${id}`} data-cy="TabLink">
+          {tab.title}
+        </a>
+      </li>))
+  }
+};
